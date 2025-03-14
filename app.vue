b598dcfbd0589a70e71b4c12aca9017b044e747d
Martin Lednár <martin.lednar@riesenia.com>
2025-03-14 20:34:21 +0100

    rToast: init

--- app.vue
+++ app.vue
@@ -1,3 +1,14 @@
+<script setup lang="ts">
+const { toast } = useToast();
+</script>
+
 <template>
-  <HelloWorld />
+  <RToaster />
+
+  <button
+    :style="{ position: 'fixed', right: '2rem', top: '2rem' }"
+    @click="toast({ title: 'Toast' })"
+  >
+    Create toast
+  </button>
 </template>
